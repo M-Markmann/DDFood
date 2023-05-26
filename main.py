426ebe1a97b2cc6d955b1b8acd711a8623fecbf5
Marius_Laptop <marius.markmann@googlemail.com>
2023-05-26 15:08:28 +0200

    Everything works

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from Discounting_Functions import *
 from  plotting_results import *
 from create_behavioral_log import behavioral_analaysis, calculate_indifference_point_list
 from log_likelihood_estimation import log_likelihood_single_parameter, log_likelihood_two_parameter
-from statistics_and_plots import statistics_and_plotting
+from statistics_and_plots import statistics_and_plotting_aic, calculate_indifference_list, statistis_and_plotting_r2
 import os
 import scipy
 
-def print_hi(name):
+def analyze_and_do_statistics():
     #define all variables necessary:
     method ='SLSQP'#
     image_folder = 'C:/Users/mariu/Documents/Arbeit/Doktorarbeit/Abbildungen/'
@@ -14,10 +14,12 @@ def print_hi(name):
     results_methods_dir = os.path.join(results_dir, method)
     if not os.path.exists(results_methods_dir):
         os.makedirs(results_methods_dir)
+    timings = [2, 14, 31, 91, 182.5, 365]
     data_path = 'C:/Users/mariu/Documents/Arbeit/DelayDiscountingFood/BehavioralDATA/'
     log_files = os.listdir(data_path)
     maxiter = 1000000
     all_data = []
+    all_data_r2 = []
     rsquared_parameter_estimates = []
     rsquared_data = []
     rsquared_data_unfiltered = []
@@ -31,11 +33,6 @@ def print_hi(name):
         events_script, indifference_liste, food_chooser, entry_counter = behavioral_analaysis(file_name, file)
         indifference_liste = np.array(indifference_liste)
         indifference_liste_with_all = indifference_liste
-        # indifference_liste = indifference_liste[indifference_liste[:,2]< 180]
-        #indifference_liste_Coin_Later = np.array(indifference_liste_Coin_Later)
-        #indifference_liste_Coin_Now = np.array(indifference_liste_Coin_Now)
-        #indifference_liste_Food_Later = np.array(indifference_liste_Food_Later)
-        #indifference_liste_Food_Now = np.array(indifference_liste_Food_Now)
         Food = indifference_liste[indifference_liste[:, 1] < 0.5]
         Money = indifference_liste[indifference_liste[:, 1] > 0.5]
         list_of_indifference_points, did_change_happened = calculate_indifference_point_list(food_chooser, entry_counter,indifference_liste_with_all)
@@ -46,16 +43,23 @@ def print_hi(name):
         Money_indifference_list = list_of_indifference_points[
            list_of_indifference_points[:, 1] > 0.5]  # Create List for all Money Decisions
 
-        bounds_two = [(0.01, 10), (0.0000001, 5)]  # beta, k
+        initial_guess_one_r2 = (0.00001)  # k
+        initial_guess_tw0_r2 = [0.0055, 0.403]
+        bounds_one_r2 = (0.00001, 1)  # k
+        bounds_two_r2 = ((0.00001, 0.01), (1, 1.5))  # k,s
+        bounds_two = [(0.1, 10), (0.00001, 1)]  # beta, k
         initial_guess_two = np.array([2, 0.055])  # beta, k
-        bounds_three = [(0.01, 10), (0.0000001, 5), (0.01,  5)]  # beta, k, s
-        initial_guess_three = np.array([2, 0.055, 1])
+        bounds_three = [(0.1, 10), (0.00001, 1), (0.01,  1.5)]  # beta, k, s
+        initial_guess_three = np.array([2, 0.055, 0.42])
 
-        discounting_functions = { single_parameter_softmax_hyperbol:2,single_parameter_softmax_exponential:2,two_parameter_softmax_green: 3, two_parameter_softmax_mazur: 3, two_parameter_softmax_prelec: 3}
+        discounting_functions_softmax = { single_parameter_softmax_hyperbol:2,single_parameter_softmax_exponential:2,two_parameter_softmax_green: 3, two_parameter_softmax_mazur: 3, two_parameter_softmax_prelec: 3}
+        discounting_functions_r = {hyberbolic_discounting:1, exponential_discounting: 1, green_meyerson_discounting: 2, mazur: 2,prelec: 2}
         conditions = [Food, Money]
-        list_of_parameters = []
+        conditions_r = [Food_indifference_list, Money_indifference_list]
+        list_of_parameters_softmax = []
+        list_of_parameters_r = []
         for condition in conditions:
-            for key, val in discounting_functions.items():
+            for key, val in discounting_functions_softmax.items():
                 if val == 2:
                     Model_Fit = scipy.optimize.minimize(log_likelihood_single_parameter,
                                                                        x0=initial_guess_two,
@@ -66,9 +70,9 @@ def print_hi(name):
                     params = [Model_Fit.x[0], Model_Fit.x[1]]
                     log_likelihood = 1/log_likelihood_single_parameter(params,condition,key)
                     aic = -2*log_likelihood+2*2
-                    list_of_parameters.append(Model_Fit.x[0])
-                    list_of_parameters.append(Model_Fit.x[1])
-                    list_of_parameters.append(aic)
+                    list_of_parameters_softmax.append(Model_Fit.x[0])
+                    list_of_parameters_softmax.append(Model_Fit.x[1])
+                    list_of_parameters_softmax.append(aic)
                 if val == 3:
                     Model_Fit = scipy.optimize.minimize(log_likelihood_two_parameter,
                                                                        x0=initial_guess_three,
@@ -79,13 +83,56 @@ def print_hi(name):
                     params = [Model_Fit.x[0], Model_Fit.x[1], Model_Fit.x[2]]
                     log_likelihood = 1 / log_likelihood_two_parameter(params, condition, key)
                     aic = -2*log_likelihood+2*3
-                    list_of_parameters.append(Model_Fit.x[0])
-                    list_of_parameters.append(Model_Fit.x[1])
-                    list_of_parameters.append(Model_Fit.x[2])
-                    list_of_parameters.append(aic)
+                    list_of_parameters_softmax.append(Model_Fit.x[0])
+                    list_of_parameters_softmax.append(Model_Fit.x[1])
+                    list_of_parameters_softmax.append(Model_Fit.x[2])
+                    list_of_parameters_softmax.append(aic)
 
-        all_data.append(list_of_parameters)
+
+        for condition in conditions_r:
+            for key, val in discounting_functions_r.items():
+                indifference_points = calculate_indifference_list(condition)
+                #list_of_parameters_r.append(counter)
+                if val == 1:
+                    def discounting_for_curve_fitting(x, k):
+                        SV = []
+                        for i in x:
+                            SV.append(key(i,k))
+                        return SV
+
+                    k_estimate, covar = curve_fit(discounting_for_curve_fitting, timings,
+                                                           indifference_points,
+                                                           p0=0.0055, maxfev=5000, bounds=bounds_one_r2)
+                    r2 = sklearn.metrics.r2_score(indifference_points,
+                                                                    discounting_for_curve_fitting(timings,k_estimate),
+                                                                    sample_weight=None)
+                    k_estimate = float(k_estimate)
+                    list_of_parameters_r.append(k_estimate)
+                    list_of_parameters_r.append(r2)
+                if val == 2:
+                    def discounting_for_curve_fitting(x, k, s):
+                        SV = []
+                        for i in x:
+                            SV.append(key(i, k,s))
+                        return SV
+
+                    parameter_estimate, covar = curve_fit(discounting_for_curve_fitting, timings,
+                                                  indifference_points,
+                                                  maxfev=5000, bounds=bounds_two_r2, p0=initial_guess_tw0_r2)
+                    k_estimate, s_estimate = parameter_estimate
+                    r2 = sklearn.metrics.r2_score(indifference_points,
+                                                  discounting_for_curve_fitting(timings, k_estimate, s_estimate),
+                                                  sample_weight=None)
+                    list_of_parameters_r.append(k_estimate)
+                    list_of_parameters_r.append(s_estimate)
+                    list_of_parameters_r.append(r2)
+
+        all_data.append(list_of_parameters_softmax)
+        all_data_r2.append(list_of_parameters_r)
+
+
     all_data = np.array(all_data)
+    all_data_r2 = np.array(all_data_r2)
     all_data =pd.DataFrame(all_data, columns=['hyperbol_Food_beta', 'hyperbol_Food_k', 'aic_Food_hyperbol',
                                               'exponential_Food_beta', 'exponential_Food_k', 'aic_Food_exponential',
                                               'meyer_green_Food_beta', 'meyer_green_Food_k', 'meyer_Food_near_s','aic_Food_meyer_green',
@@ -98,12 +145,20 @@ def print_hi(name):
                                               'prelec_Money_beta', 'prelec_Money_k', 'prelec_Money_s', 'aic_Money_prelec'
                                               ])
 
-    statistics_and_plotting(all_data)
+
+    r2_data = pd.DataFrame(all_data_r2, columns=['k_food_hyperbol','r2_hyberbole_food','k_food_exponential','r2_exponential_food',
+                                                 'k_food_gm','s_food_gm','r2_GM_food','k_food_mazur','s_food_mazur','r2_mazur_food','k_food_prelec','s_food_prelec','r2_prelec_food',
+                                                   'k_money_hyperbol','r2_hyberbole_money','k_money_exponential','r2_exponential_money',
+                                                 'k_money_gm','s_money_gm','r2_GM_money','k_money_mazur','s_money_mazur','r2_mazur_money','k_money_prelec','s_money_prelec','r2_prelec_money',
+                                                  ])
+    'r2_exponential_money', 'r2_hyberbole_money', 'r2_GM_money', 'r2_mazur_money', 'r2_prelec_money'
+    statistics_and_plotting_aic(all_data)
+    statistis_and_plotting_r2(r2_data)
 
 
 
 
         if __name__ == '__main__':
-    print_hi('PyCharm')
+    analyze_and_do_statistics()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
--- statistics_and_plots.py
+++ statistics_and_plots.py
@@ -1,9 +1,78 @@
 import seaborn as sns
+import numpy as np
 from matplotlib.ticker import FormatStrFormatter
+import pandas as pd
+import scipy
 from matplotlib.ticker import StrMethodFormatter
 
+def get_wilcoxon_rank_and_make_fancy_graphics(df,significance_value):
+    df = df
+    dfcols = pd.DataFrame(columns=df.columns)
+    pvalues = dfcols.transpose().join(dfcols, how="outer")
+    correlations = dfcols.transpose().join(dfcols, how="outer")
+    correlations = correlations.to_numpy()
+    pvalues = pvalues.to_numpy()
+    df_numpy = df.to_numpy()
+    for ix in range(np.size(df_numpy, 1)):
+        for jx in range(np.size(df_numpy, 1)):
+            if ix is not jx:
+                sp = scipy.stats.wilcoxon(df_numpy[:, ix], df_numpy[:, jx])
+                correlations[ix, jx] = sp[0]
+                pvalues[ix, jx] = sp[1]  # Only store values below the diagonal
+            else:
+                correlations[ix, jx] = 1
+                pvalues[ix, jx] = 1
 
-def statistics_and_plotting(all_data):
+    uncorrected_p_values = pd.DataFrame(pvalues,
+                                        columns=['Exponential', 'Hyperbole', 'Hyperbole \n with time scaling \n Delay', 'Hyperbole \n with scaling \n Delay & Discounting',
+     'Exponential \n with time scaling'], #df.columns.values.tolist(),
+                                        index=['Exponential', 'Hyperbole', 'Hyperbole \n with time scaling \n Delay', 'Hyperbole \n with scaling \n Delay & Discounting',
+     'Exponential \n with time scaling'], dtype="float")
+
+
+    g = sns.clustermap(uncorrected_p_values, cmap=sns.dark_palette("#D2042D",reverse=True), vmin=0, vmax=0.1, row_cluster = False, col_cluster = False)
+    #"rocket"
+    #plt.xticks([0.5, 1.5])
+    #plt.yticks([0.5, 1.5])
+    #plt.title('Confusion matrix')
+    #plt.xlabel('Actual label')
+    #plt.ylabel('Predicted label');
+
+    # Here labels on the y-axis are rotated
+    for tick in g.ax_heatmap.get_yticklabels():
+        tick.set_rotation(0)
+    as_numpy_uncorrected = uncorrected_p_values.to_numpy()
+    counter = [0,1,2,3,4]
+    # Here we add asterisks onto cells with signficant correlations
+    for i, ix in enumerate(counter):#enumerate(g.dendrogram_row.reordered_ind):
+        for j, jx in enumerate(counter):#enumerate(g.dendrogram_row.reordered_ind):
+            if i != j:
+                if as_numpy_uncorrected[ix, jx] <= significance_value and as_numpy_uncorrected [ix, jx]> significance_value/10:
+                    text_value = "*"
+                elif  as_numpy_uncorrected[ix, jx] <= significance_value/10  and as_numpy_uncorrected [ix, jx]> significance_value/1000:
+                    text_value = "**"
+                elif  as_numpy_uncorrected[ix, jx] <= significance_value/1000 and as_numpy_uncorrected [ix, jx]> significance_value/10000:
+                    text_value = "***"
+                elif  as_numpy_uncorrected[ix, jx] <= significance_value/10000:
+                    text_value = "****"
+                else:
+                    text_value = ""
+                text = g.ax_heatmap.text(
+                    j + 0.5,
+                    i + 0.5,
+                    text_value,
+                    ha="center",
+                    va="center",
+                    color="white",
+                )
+                text.set_fontsize(18)
+    g.ax_heatmap.set_xticklabels(g.ax_heatmap.get_xmajorticklabels(), fontsize=18)
+    g.ax_heatmap.set_yticklabels(g.ax_heatmap.get_ymajorticklabels(), fontsize=18)
+    return g
+
+
+
+def statistics_and_plotting_aic(all_data):
     min_food = all_data[
         ['aic_Food_hyperbol', 'aic_Food_exponential', 'aic_Food_meyer_green', 'aic_Food_mazur', 'aic_Food_prelec']].min(
         axis=1)
@@ -39,7 +108,7 @@ def statistics_and_plotting(all_data):
     ax.axhline(y=2, color='black', linestyle='--', linewidth=3)
     ax.set(ylim=(0, 11))
     ax.yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
-    ax.savefig('DAIC_FOOD.png')
+
 
     sns.set(rc={'figure.figsize': (22, 8.27)})
     sns.set(font_scale=3)
@@ -61,3 +130,64 @@ def statistics_and_plotting(all_data):
     ax.set(ylim=(0, 11))
 
     return True
+
+def statistis_and_plotting_r2(r2_data):
+    my_pal_money = {"r2_exponential_money": "darkmagenta", "r2_hyberbole_money": "limegreen", "r2_mazur_money": "fuchsia" ,"r2_GM_money":"darkgoldenrod",'r2_prelec_money':'royalblue'}
+    sns.set(font_scale=3)
+    ax = sns.barplot(data=r2_data[
+        ['r2_exponential_money','r2_hyberbole_money', 'r2_GM_money', 'r2_mazur_money','r2_prelec_money']],  palette=my_pal_money)
+    ax.set_xticklabels(
+        ['Exponential', 'Hyperbole', 'Hyperbole \n with time scaling \n Delay',
+         'Hyperbole \n with scaling \n Delay & Discounting',
+         'Exponential \n with time scaling'])
+    ax.set(ylim=(0, 1))
+    #'r2_exponential_money', 'r2_hyberbole_money',
+    ax.set_xlabel("Model Fit for the Money condition")
+    ax.set_ylabel('R\u00b2')
+    ax.patches[2].set_edgecolor('red')
+    ax.patches[2].set_linewidth(5)
+    ax.set(ylim=(0, 1))
+
+    sns.set(rc={'figure.figsize': (22, 11)})
+    sns.set(font_scale=2.2)
+    my_pal_food = {"r2_exponential_food": "firebrick", "r2_hyberbole_food": "darkgoldenrod",
+                   "r2_mazur_food": "darkcyan", "r2_GM_food": "limegreen",
+                   'r2_prelec_food': 'darkmagenta'}
+    ax = sns.barplot(data=r2_data[
+        ['r2_exponential_food', 'r2_hyberbole_food', 'r2_GM_food',
+         'r2_mazur_food', 'r2_prelec_food']], palette=my_pal_food)
+    ax.set_xticklabels(
+        ['Exponential', 'Hyperbole', 'Hyperbole \n with time scaling \n Delay',
+         'Hyperbole \n with scaling \n Delay & Discounting',
+         'Exponential \n with time scaling'])
+    ax.set_xlabel("Model Fit for the Money condition")
+    ax.set_ylabel('R\u00b2')
+    ax.patches[3].set_edgecolor('red')
+    ax.patches[3].set_linewidth(5)
+    ax.patches[4].set_edgecolor('red')
+    ax.patches[4].set_linewidth(5)
+    # ax.axhline(y=2, color='black', linestyle='--')
+    ax.set(ylim=(0, 1))
+
+    get_wilcoxon_rank_and_make_fancy_graphics(r2_data[['r2_exponential_food', 'r2_hyberbole_food', 'r2_GM_food',
+         'r2_mazur_food', 'r2_prelec_food']],0.05)
+    get_wilcoxon_rank_and_make_fancy_graphics(r2_data[['r2_exponential_money','r2_hyberbole_money', 'r2_GM_money', 'r2_mazur_money','r2_prelec_money']], 0.05)
+
+    return True
+
+
+
+def calculate_indifference_list(List_of_decisions):
+    decisions_run_1 = []
+    decisions_run2 = []
+    for i in range(int(len(List_of_decisions))):
+        if i % 2 == 0:
+            if List_of_decisions[i, 0] > List_of_decisions[i + 1, 0]:
+                decisions_run_1.append(List_of_decisions[i + 1, 3])
+                decisions_run2.append(List_of_decisions[i, 3])
+            else:
+                decisions_run_1.append(List_of_decisions[i, 3])
+                decisions_run2.append(List_of_decisions[i + 1, 3])
+    indifference_points_list = (np.array(decisions_run_1) + np.array(decisions_run2))/2
+
+    return indifference_points_list
